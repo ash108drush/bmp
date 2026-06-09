72e4c5f7307bf9370dcc6f70a189962e4d231e97
ash108drush <47239895+ash108drush@users.noreply.github.com>
2026-06-09 22:28:03 +0300

    Add files via upload

---
+++ ImgLib/build/Desktop-Debug/CMakeFiles/ImgLib.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ash/Dev/ImgLib/bmp_image.cpp" "CMakeFiles/ImgLib.dir/bmp_image.cpp.o" "gcc" "CMakeFiles/ImgLib.dir/bmp_image.cpp.o.d"
+  "/home/ash/Dev/ImgLib/img_lib.cpp" "CMakeFiles/ImgLib.dir/img_lib.cpp.o" "gcc" "CMakeFiles/ImgLib.dir/img_lib.cpp.o.d"
+  "/home/ash/Dev/ImgLib/jpeg_image.cpp" "CMakeFiles/ImgLib.dir/jpeg_image.cpp.o" "gcc" "CMakeFiles/ImgLib.dir/jpeg_image.cpp.o.d"
+  "/home/ash/Dev/ImgLib/ppm_image.cpp" "CMakeFiles/ImgLib.dir/ppm_image.cpp.o" "gcc" "CMakeFiles/ImgLib.dir/ppm_image.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
